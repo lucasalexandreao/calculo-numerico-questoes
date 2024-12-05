44e6bb6f0448cd677df89b899f924b5aa7328f15
lauramoroni <moronilaura28@gmail.com>
2024-12-05 10:55:35 -0300

    finish q8

---
+++ prova1/q8.py
@@ -0,0 +1,40 @@
+from math import pi, sqrt
+#from q6 import ponto_fixo
+
+# Função para área S
+def f(r, h = 20):
+    return pi * r * sqrt(r**2 + h**2)
+
+# Função de iteração    
+def g(r, h = 20, s = 1200):
+    return s / (pi * sqrt(r**2 + h**2))
+
+# Método do Ponto Fixo
+def ponto_fixo(a, tol, iter_max):
+    c = a # Ponto inicial
+    gc = g(c)
+    i = 0
+
+    while abs(f(gc)) > tol and i < iter_max - 1:
+        c = gc
+        gc = g(c)
+        i += 1
+
+    print(f"Iterações: {i}")
+    print(f"Raio: {gc:.5f}")
+
+# main
+if __name__ == "__main__":
+    # Valores de entrada
+    r = 17 # Raio inicial
+    iter_max = 5
+    tol = float(input("Digite o valor da tolerância: "))
+    
+    # Método do ponto fixo
+    print("\n== Ponto Fixo ==")
+ 
+    ponto_fixo(r, tol, iter_max)
+    
+        
+        
+    
