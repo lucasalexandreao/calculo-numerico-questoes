904bb0fe5519ec5ecfe3aed48a33baf901006f12
lauramoroni <moronilaura28@gmail.com>
2024-12-05 10:06:44 -0300

    método ponto fixo em função

--- prova1/q4.py
+++ prova1/q4.py
@@ -4,60 +4,71 @@ from time import perf_counter
 def f(x):
     return (x - 1)**2
 
-a = float(input("Primeiro número do intervalo: "))
-b = float(input("Segundo número do intervalo: "))
-epsilon = 0.0001
-raiz = None
-
-k = ceil((log(b-a, 10) - log(epsilon, 10)) / log(2, 10))
-iteracoes = 0
-pontos = [a, b]
-pms = []
-
-start_time = perf_counter()
-
-if abs(f(a)) < epsilon:
-    raiz = a
-elif abs(f(b)) < epsilon:
-    raiz = b
-
-while iteracoes <= k and raiz is None:
-    for idx in range(len(pontos) - 1):
-        p1 = pontos[idx]
-        p2 = pontos[idx + 1]
-        if f(p2) - f(p1) != 0:
-            pm = (p1*f(p2) - p2*f(p1)) / (f(p2) - f(p1))
-        else:
-           raise Exception("Este intervalo não é válido, pois resulta numa divisão por zero.")
-        if abs(f(pm)) < epsilon:
-            raiz = pm
-            break
-        elif raiz is None:
-            if pm not in pontos and pm not in pms:
-                pms.append(pm)
-            if f(p1) * f(p2) < 0:
-                while abs(p2 - p1) > epsilon:
-                    if abs(f(pm)) < epsilon:
-                        raiz = pm
-                        break
-                    elif f(p1) * f(pm) < 0:
-                        p2 = pm
-                    else:
-                        p1 = pm
-                    if abs(p2 - p1) > epsilon:
-                        pm = (p1*f(p2) - p2*f(p1)) / (f(p2) - f(p1))
-                        iteracoes += 1
-                raiz = pm
-        if raiz is not None:
-            break
-    iteracoes += 1
-    pontos += pms
+def falsa_posicao(a, b, epsilon):
+    
+    k = ceil((log(b-a, 10) - log(epsilon, 10)) / log(2, 10))
+    iteracoes = 0
+    pontos = [a, b]
     pms = []
-    pontos.sort()
+    
+    if abs(f(a)) < epsilon:
+        raiz = a
+    elif abs(f(b)) < epsilon:
+        raiz = b
 
-end_time = perf_counter()
+    while iteracoes <= k and raiz is None:
+        for idx in range(len(pontos) - 1):
+            p1 = pontos[idx]
+            p2 = pontos[idx + 1]
+            if f(p2) - f(p1) != 0:
+                pm = (p1*f(p2) - p2*f(p1)) / (f(p2) - f(p1))
+            else:
+               raise Exception("Este intervalo não é válido, pois resulta numa divisão por zero.")
+            if abs(f(pm)) < epsilon:
+                raiz = pm
+                break
+            elif raiz is None:
+                if pm not in pontos and pm not in pms:
+                    pms.append(pm)
+                if f(p1) * f(p2) < 0:
+                    while abs(p2 - p1) > epsilon:
+                        if abs(f(pm)) < epsilon:
+                            raiz = pm
+                            break
+                        elif f(p1) * f(pm) < 0:
+                            p2 = pm
+                        else:
+                            p1 = pm
+                        if abs(p2 - p1) > epsilon:
+                            pm = (p1*f(p2) - p2*f(p1)) / (f(p2) - f(p1))
+                            iteracoes += 1
+                    raiz = pm
+            if raiz is not None:
+                break
+            
+        iteracoes += 1
+        pontos += pms
+        pms = []
+        pontos.sort()
+        
+    return raiz, iteracoes, k
+    
+# main
+if __name__ == "__main__":
 
-if raiz is not None:
-    print(f"Raiz: {raiz:.5f}\nIterações: {iteracoes}, Limite: {k}\nTempo: {end_time - start_time}")
-elif iteracoes > k:
-    print(f"Esta função não tem raiz.\nIterações: {iteracoes}, Limite: {k}\nTempo: {end_time - start_time}")
+    a = float(input("Primeiro número do intervalo: "))
+    b = float(input("Segundo número do intervalo: "))
+    epsilon = float(input("Epsilon: "))
+    raiz = None
+
+    start_time = perf_counter()
+    raiz, iteracoes, k = falsa_posicao(a, b, epsilon)
+    end_time = perf_counter()
+
+    if raiz is not None:
+
+        print(f"Raiz: {raiz:.5f}\nIterações: {iteracoes}, Limite: {k}\nTempo: {end_time - start_time}")
+    elif iteracoes > k:
+        print(f"Esta função não tem raiz.\nIterações: {iteracoes}, Limite: {k}\nTempo: {end_time - start_time}")
+
+
