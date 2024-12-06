0d94fde137fece41e5a7ead706c5eb20604c8182
Lucas Alexandre <lucasparaoif@gmail.com>
2024-12-05 21:23:33 -0300

    ajuste em q6.py

--- prova1/q6.py
+++ prova1/q6.py
@@ -9,6 +9,53 @@ def f(x):
 def g(x):
     return 6.0 / (x + 1.0)
 
+
+def metodo_bissecao(a, b, epsilon):
+    raiz = None
+    iteracoes = 0
+    pontos = [a, b]
+    pms = []
+
+    k = ceil((log(b - a, 10) - log(epsilon, 10)) / log(2, 10))
+
+    if abs(f(a)) < epsilon:
+        raiz = a
+    elif abs(f(b)) < epsilon:
+        raiz = b
+
+    while iteracoes <= k and raiz is None:
+        for idx in range(len(pontos) - 1):
+            p1 = pontos[idx]
+            p2 = pontos[idx + 1]
+            pm = (p1 + p2) / 2
+            if abs(f(pm)) < epsilon:
+                raiz = pm
+                break
+            elif raiz is None:
+                pms.append(pm)
+                if f(p1) * f(p2) < 0:
+                    while abs(p2 - p1) > epsilon:
+                        if abs(f(pm)) < epsilon:
+                            raiz = pm
+                            break
+                        elif f(p1) * f(pm) < 0:
+                            p2 = pm
+                        else:
+                            p1 = pm
+                        if abs(p2 - p1) > epsilon:
+                            pm = (p1 + p2) / 2
+                            iteracoes += 1
+                    raiz = pm
+            if raiz is not None:
+                break
+        iteracoes += 1
+        pontos += pms
+        pms = []
+        pontos.sort()
+
+    return raiz, iteracoes, k
+
+
 # Método do Ponto Fixo
 def ponto_fixo(a, tol):
     c = a # Ponto inicial
@@ -40,7 +87,10 @@ if __name__ == "__main__":
     # Método do ponto fixo
     print("\n== Ponto Fixo ==")
     ponto_fixo(a, tolerancia)
-    
+
+
     # Método da bisseção
     print("\n== Método  da bisseção ==")
-    metodo_bissecao(a, b, tolerancia)
+    raiz_bisseccao, iteracoes_bisseccao, k = metodo_bissecao(a, b, tolerancia)
+    print(f"Iterações: {iteracoes_bisseccao}")
+    print(f"Raiz aproximada: {raiz_bisseccao:.5f}")
