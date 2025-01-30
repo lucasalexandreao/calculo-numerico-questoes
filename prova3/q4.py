0cf151a61d4f075ef64e645d2bc13209b17ef0c1
Lucas Alexandre <lucasparaoif@gmail.com>
2025-01-30 19:36:31 -0300

    update q4

--- prova3/q4.py
+++ prova3/q4.py
@@ -8,15 +8,14 @@ O objetivo é calcular o consumo total de CPU ao longo do tempo, que pode ser ob
 no intervalo de tempo [0,8].
 """
 
-from simpson8 import simpson8_table
-
+from trapezoidal_rule import trapezoidal_rule_table
 
 def main():
     # Tabelas
     time = [0, 1, 2, 3, 4, 5, 6, 7, 8] # Tempo em segundos
     cpu_use = [0, 20, 50, 80, 100, 70, 40, 10, 0] # Uso da CPU em porcentagem
 
-    result = simpson8_table(time, cpu_use)
+    result = trapezoidal_rule_table(time, cpu_use)
     print(f"Consumo total da CPU: {result} %·s")
 
 
